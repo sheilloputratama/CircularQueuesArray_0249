895f2f110cc919ded56783674c2a82f50ebf260b
sheilloputratama <sheilloggendut@gmail.com>
2025-06-10 11:07:11 +0700

    membuat cek apakah antrian penuh

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -17,4 +17,10 @@ public:
         cin >> num;
         cout << endl;
 
-       
+        // cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max -1)|| (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow";
+            return;
+        }
+        
