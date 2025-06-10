012586c55c5cd1f1c2292cd66deaa5ba14bb4553
sheilloputratama <sheilloggendut@gmail.com>
2025-06-10 11:07:47 +0700

    membuat cek apakah antrian kosong

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -23,4 +23,10 @@ public:
             cout << "\nQueue overflow";
             return;
         }
+        // cek apakah antrian kosong 
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
         
