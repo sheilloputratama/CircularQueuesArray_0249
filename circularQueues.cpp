22000bee794f9b0d6a087e27601abae5713941c7
sheilloputratama <sheilloggendut@gmail.com>
2025-06-10 11:35:30 +0700

    Iterasi dari awal array hingga REAR

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -96,4 +96,11 @@ public:
             }
             FRONT_position = 0;
 
-           
+            // Iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            
+          
